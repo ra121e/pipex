ac97b089977b8b9ab22727f9c6f620846f28ad95
root <sushi.honda@gmail.com>
2024-08-10 00:50:49 +0800

    execve function test

---
+++ pipex.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipx.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/08 23:36:32 by athonda           #+#    #+#             */
+/*   Updated: 2024/08/10 00:47:02 by athonda          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char **argv, char **envp)
+{
+	char	*ptr[3];
+
+	(void)argc;
+	(void)argv;
+	ptr[0] = ""; //"cat"でも"./a.out"でも何でも良い
+	ptr[1] = "test.txt";
+	ptr[2] = NULL;
+	printf("process ID: %d\n", getpid());
+	execve("/usr/bin/cat", ptr, envp);
+	printf("process ID: %d\n", getpid());
+	return (0);
+}
