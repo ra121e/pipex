0ea2c3fa0e9fb8e2f3d695e5c6a7002b62c4d9fb
root <sushi.honda@gmail.com>
2024-08-11 14:36:46 +0800

    fork: execve in child: wait until child end

--- pipex.c
+++ pipex.c
@@ -6,11 +6,12 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 23:36:32 by athonda           #+#    #+#             */
-/*   Updated: 2024/08/11 13:06:09 by athonda          ###   ########.fr       */
+/*   Updated: 2024/08/11 14:27:08 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <sys/wait.h>
 #include <stdio.h>
 
 extern char	**environ;
@@ -18,6 +19,8 @@ extern char	**environ;
 int main(int argc, char **argv, char **envp)
 {
 	char	*ptr[3];
+	pid_t	pid;
+	int		status;
 
 	(void)argc;
 	(void)argv;
@@ -26,7 +29,22 @@ int main(int argc, char **argv, char **envp)
 	ptr[2] = NULL;
 	printf("process ID: %d\n", getpid());
 	printf("environment variable: %s\n", environ[0]);
-	execve("/usr/bin/cat", ptr, envp);
-	printf("process ID: %d\n", getpid());
+	printf("This is the before fork\n");
+	pid = fork();
+	printf("This is the after fork\n");
+	printf("process ID after fork: %d\n", getpid());
+	if (pid == 0)
+	{
+		printf("This is the first in child process\n");
+		printf("process ID in child: %d\n", getpid());
+		execve("/usr/bin/cat", ptr, envp);
+		printf("process ID in child after execve: %d\n", getpid());
+		_exit(0);
+	}
+	else if (pid > 0)
+	{
+		wait(&status);
+		printf("wait for end of child process\n");
+	}
 	return (0);
 }
