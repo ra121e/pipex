3010233a02aadc1fb3c185fba4bc1f9a411b2bdf
root <sushi.honda@gmail.com>
2024-08-11 20:00:18 +0800

    dup to redirect to file

--- pipex.c
+++ pipex.c
@@ -6,40 +6,30 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 23:36:32 by athonda           #+#    #+#             */
-/*   Updated: 2024/08/11 14:27:08 by athonda          ###   ########.fr       */
+/*   Updated: 2024/08/11 19:58:00 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 #include <stdio.h>
-
-extern char	**environ;
 
 int main(int argc, char **argv, char **envp)
 {
-	char	*ptr[3];
 	pid_t	pid;
 	int		status;
+	int		fd;
 
 	(void)argc;
 	(void)argv;
-	ptr[0] = ""; //"cat"でも"./a.out"でも何でも良い
-	ptr[1] = "test.txt";
-	ptr[2] = NULL;
-	printf("process ID: %d\n", getpid());
-	printf("environment variable: %s\n", environ[0]);
-	printf("This is the before fork\n");
 	pid = fork();
-	printf("This is the after fork\n");
-	printf("process ID after fork: %d\n", getpid());
 	if (pid == 0)
 	{
-		printf("This is the first in child process\n");
-		printf("process ID in child: %d\n", getpid());
-		execve("/usr/bin/cat", ptr, envp);
-		printf("process ID in child after execve: %d\n", getpid());
-		_exit(0);
+		fd = open("./test.txt", O_WRONLY);
+		dup2(fd, 1);
+		if (execve("/usr/bin/ls", argv, envp) == -1)
+			_exit (0);
 	}
 	else if (pid > 0)
 	{
