bef790ac5050adea97ceeb1adfba8733e6dff036
root <sushi.honda@gmail.com>
2024-08-19 07:41:56 +0800

    add message

--- pipex.c
+++ pipex.c
@@ -6,11 +6,25 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 23:36:32 by athonda           #+#    #+#             */
-/*   Updated: 2024/08/16 16:11:58 by athonda          ###   ########.fr       */
+/*   Updated: 2024/08/17 21:44:52 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @file pipex.c
+ * @brief run two shell commands by connected by pipe
+ */
+
 #include "pipex.h"
+
+/**
+ * @fn void	child
+ * @brief child process swich fd and call execute command function
+ * @param[in] argv
+ * @param[in] envp
+ * @param[in] pipfd array of fd of pipe
+ * @return none
+ */
 
 void	child(char **argv, char **envp, int *pipfd)
 {
@@ -22,7 +36,6 @@ void	child(char **argv, char **envp, int *pipfd)
 	fd = open(argv[1], O_RDONLY, 0777);
 	if (dup2(fd, STDIN_FILENO) < 0)
 		perror("dup2 pipe read -> standard IN: error in child");
-	//close(pipfd[1]);
 	exec_cmd(argv[2], envp);
 }
 
@@ -36,8 +49,6 @@ void	parent(char **argv, char **envp, int *pipfd)
 	fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	if (dup2(fd, STDOUT_FILENO) < 0)
 		perror("dup2 pipe write -> standard OUT: error in parent");
-	//close(pipfd[0]);
-	printf("wait for end of child process\n");
 	exec_cmd(argv[3], envp);
 }
 
@@ -56,7 +67,6 @@ void	pipex(int argc, char **argv, char **envp)
 		child(argv, envp, pipfd);
 	else if (pid > 0)
 		parent(argv, envp, pipfd);
-
 }
 
 int main(int argc, char **argv, char **envp)
