15c10eafd27c4852d70ae82bd14e101da367318e
root <sushi.honda@gmail.com>
2024-08-20 13:06:07 +0800

    error handling: put exit(1) in each error case

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 23:36:32 by athonda           #+#    #+#             */
-/*   Updated: 2024/08/19 14:42:36 by athonda          ###   ########.fr       */
+/*   Updated: 2024/08/20 13:04:20 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,18 @@ void	child(char **argv, char **envp, int *pipfd)
 
 	close(pipfd[0]);
 	if (dup2(pipfd[1], STDOUT_FILENO) < 0)
-		perror("dup2 pipe write -> standard out: error in child");
+	{
+		perror("dup2 pipe write end -> standard out: error in child");
+		exit(1);
+	}
 	fd = open(argv[1], O_RDONLY, 0777);
+	if (fd < 0)
+		exit(1);
 	if (dup2(fd, STDIN_FILENO) < 0)
-		perror("dup2 pipe read -> standard IN: error in child");
+	{
+		perror("dup2 pipe read end -> standard IN: error in child");
+		exit(1);
+	}
 	exec_cmd(argv[2], envp);
 }
 
@@ -45,10 +53,16 @@ void	parent(char **argv, char **envp, int *pipfd)
 
 	close(pipfd[1]);
 	if (dup2(pipfd[0], STDIN_FILENO) < 0)
+	{
 		perror("dup2 pipe read -> standard IN: error in parent");
+		exit(1);
+	}
 	fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	if (dup2(fd, STDOUT_FILENO) < 0)
+	{
 		perror("dup2 pipe write -> standard OUT: error in parent");
+		exit(1);
+	}
 	exec_cmd(argv[3], envp);
 }
 
@@ -59,7 +73,10 @@ void	pipex(int argc, char **argv, char **envp)
 
 	(void)argc;
 	if (pipe(pipfd) == -1)
+	{
 		perror("pip creation error");
+		exit(1);
+	}
 	pid = fork();
 	if (pid < 0)
 		perror("fork error!");
