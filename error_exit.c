1778d50099329243cebdfa4f095f001f5f19d924
root <sushi.honda@gmail.com>
2024-08-22 16:10:36 +0800

    norm

--- error_exit.c
+++ error_exit.c
@@ -6,11 +6,23 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 17:46:53 by athonda           #+#    #+#             */
-/*   Updated: 2024/08/21 16:18:49 by athonda          ###   ########.fr       */
+/*   Updated: 2024/08/22 07:54:29 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @file error_exit.c
+ * @brief perror and exit with failure
+ */
+
 #include "pipex.h"
+
+/**
+ * @fn void	error_exit(char *str)
+ * @brief perror and exit
+ * @param[in] *str error message
+ * @return none
+ */
 
 void	error_exit(char *str)
 {
